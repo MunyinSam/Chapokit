e9ace3c4da40365b216c67162c994688ec474009
MunyinSam <kelvinsam233@gmail.com>
2023-12-18 22:02:53 +0700

    First file

---
+++ db.py
@@ -0,0 +1,122 @@
+from datetime import datetime, timezone
+from bson import ObjectId
+from mongoengine import Document, fields, connect
+import discord
+from discord import app_commands
+from discord.ext import commands
+import random
+
+# Connect to MongoDB
+connect('bank_accounts', host='mongodb+srv://Munyin:Kelvinsam1@cluster0.bviirp8.mongodb.net/?retryWrites=true&w=majority')
+
+
+# maindb--------------------------------
+
+class Player(Document):
+    name = fields.StringField(required=True)
+    money = fields.IntField()
+    stealCount = fields.IntField()
+
+def create_player(name):
+    player_data = {
+        'name': name,
+        'money' : 1000,
+        'stealCount' : 0
+    }
+    player = Player(**player_data)
+    player.save()
+
+#-----------------------------------------
+
+def get_stats(player_name):
+    #print(player_name)
+    player = Player.objects(name=player_name).first()
+    
+    if player:
+        name = player.name
+        balance = player.money
+        stealCount = player.stealCount
+        
+        return {
+            "name": name,
+            "balance": balance,
+            "stealCount": stealCount
+        }
+    else:
+        return None
+
+def steal(author, victim, amount):
+    # Retrieve the player initiating the steal
+    player_author = Player.objects(name=author).first()
+
+    # Retrieve the victim
+    player_victim = Player.objects(name=victim).first()
+
+    if player_author and player_victim:
+        # Check if the author has attempted too many steals
+
+        # Increment the steal count for the author
+        player_author.stealCount += 1
+        player_author.save()
+
+        stolen_amount = amount  
+
+        # Check if the victim has enough balance to be stolen
+        if player_victim.money >= stolen_amount:
+            player_author.money += stolen_amount
+            player_victim.money -= stolen_amount
+
+            if stolen_amount == 1000:
+                return f"JACKPOT!!! You successfully stole {stolen_amount} from {victim}!"
+            elif stolen_amount == 600:
+                return f"LUCKY!!! You successfully stole {stolen_amount} from {victim}!" 
+
+            # Save changes to the database
+            player_author.save()
+            player_victim.save()
+
+            return f"You successfully stole {stolen_amount} from {victim}!"
+        else:
+            return f"{victim} does not have enough money to be stolen."
+    else:
+        return "Invalid player names. Make sure both players exist."
+
+def steal_chance():
+
+    chance = random.randint(0,100)
+
+    if chance < 50:
+        return "50"
+    elif chance >= 50 and chance < 70:
+        return "100"
+    elif chance >= 70 and chance < 80:
+        return "150"
+    elif chance >= 70 and chance < 80:
+        return "200"
+    elif chance >= 80 and chance < 90:
+        return "400"
+    elif chance >= 90 and chance < 99:
+        return "600"
+    elif chance == 100:
+        return "1000"
+       
+def auction():
+
+    random_number = random.randint(1, 99)
+    result = str(random_number) + "000"
+    pricepool = int(result)
+    return pricepool
+
+
+# Check ----------------------------------------
+
+def check_if_exist(player_name):
+
+    player = Player.objects(name=player_name).first()
+
+    if player:
+        print("Player already Exist")
+        return True
+    else:
+        print("Player doesn't Exist")
+        return False
---
+++ main.py
@@ -0,0 +1,104 @@
+import discord
+from discord.ui import Button, View
+from discord import app_commands
+from discord.ext import commands
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+
+from db import *
+
+#----------------------------------------------------------------------
+
+print("Current Working Directory:", os.getcwd())
+
+uri = f"mongodb+srv://Munyin:Kelvinsam1@cluster0.bviirp8.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+#----------------------------------------------------------------------
+
+@bot.command(name="crbank")
+async def create_id(ctx):
+
+    if ctx.channel.id == 1185882637093588992:  # Replace with the desired channel ID
+        
+        member = ctx.message.author
+        player_exist = check_if_exist(f"{member}")
+
+        if player_exist == True:
+            embed = discord.Embed(title=f"@{member} Bank Account Already Exist", color= discord.Color.green())
+            await ctx.send(embed=embed)   
+
+        else:
+            create_player(f"{member}")
+            player_stats = get_stats(f"{member}")
+
+            embed = discord.Embed(title=f"@{member} Bank Account Created", color= discord.Color.green())
+            embed.set_thumbnail(url=member.avatar)
+            embed.add_field(name="Name", value=f"{player_stats['name']}")
+            embed.add_field(name="Balance", value=f'{player_stats['balance']}')
+            embed.add_field(name="Crimes", value=f'{player_stats['stealCount']}')
+            await ctx.send(embed=embed)
+
+        channel = bot.get_channel(1185891079468363796)
+
+        # Check if the channel exists
+        if channel:
+            # Send the message to the specified channel
+            await channel.send(f'{member} successfully created a Bank Account')
+
+    else:
+        await ctx.send('This command can only be used in "bank".')
+
+@bot.command(name="steal")
+async def stealing(ctx, victim: discord.Member):
+
+    if ctx.channel.id == 1185882637093588992:
+        victim_name = victim.name.lower()
+        author_name = ctx.message.author.name.lower()
+
+        luck = int(steal_chance())
+
+        result = steal(author_name, victim_name, luck)
+        await ctx.send(result)
+    
+        channel = bot.get_channel(1185891079468363796)
+
+        # Check if the channel exists
+        if channel:
+            # Send the message to the specified channel
+            await channel.send(f'{author_name} successfully stolen {luck} from {victim_name}')
+
+@bot.command(name='bal')
+async def check_balance(ctx):
+
+    if ctx.channel.id == 1185882637093588992:
+        member = ctx.message.author
+        bankacc = get_stats(f'{member}')
+
+        embed = discord.Embed(title=f"@{member} Balance", color= discord.Color.green())
+        embed.set_thumbnail(url=member.avatar)
+        embed.add_field(name="Money Left", value=f"{bankacc['balance']}",inline=False)
+        embed.add_field(name="Crimes", value=f"{bankacc['stealCount']}",inline=False)
+
+        await ctx.send(embed=embed)
+    
+@bot.command(name='auc')
+async def auction_command(ctx):
+    
+    price = auction()
+    await ctx.send(price)
+
+
+
+#-----------------------------------------------------------------
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+bot.run("MTE3OTY5MzkzNzYzNjY5MjAyOA.GLW1yX.5as--zLkWPNYgcQP2DBREYHTc_WWBzHn2_TaQI")
